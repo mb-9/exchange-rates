d7ff54e9e602404330320c2026144afa1cc55c29
Michaela <kvapka.kvapla@gmail.com>
2024-01-09 19:38:23 +0100

    fetching countries, fixed field

--- app/Http/Controllers/CountryController.php
+++ app/Http/Controllers/CountryController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Country;
 use Illuminate\View\View;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Http;
@@ -30,12 +31,58 @@ class CountryController extends Controller
 
     public function fetch() 
     {
-
        
         $response = Http::get(Config::get('app.restCountriesUrl'), 
-        ['fields' =>  'name,capital,currencies,population,timezones,flags,currencies']);
+        ['fields' =>  'name,capital,currencies,population,timezones,flags']);
+        $arrCountries = $response->json();
 
-        dd($response->json());
+        foreach($arrCountries as $country)
+        {
+
+            $newCountry = new Country();
+
+            if(isset($country['name']['common']))
+                $newCountry->commonName = $country['name']['common'];
+
+            if(isset($country['name']['official']))
+                $newCountry->officialName = $country['name']['official'];
+
+            if(isset($country['capital'][0]))
+                $newCountry->capital = $country['capital'][0];
+            
+            if(isset($country['population']))
+                $newCountry->population = $country['population'];
+
+            if(isset($country['timezones'][0]))
+                $newCountry->timezone = $country['timezones'][0];
+
+            if(isset($country['flags']['png']))
+                $newCountry->flagUrl = $country['flags']['png'];
+            
+            if(isset($country['currencies']))
+            {
+
+                $currencyCode   = NULL;
+                $arrKeys        = array_keys($country['currencies']);
+                
+                if(isset($arrKeys[0]))
+                    $currencyCode = $arrKeys[0];
+                
+                $newCountry->currencyCode = $currencyCode;
+
+                if(isset($country['currencies'][$currencyCode]['symbol']))
+                    $newCountry->currencySymbol = $country['currencies'][$currencyCode]['symbol'];
+            
+            }
+
+            //TODO: validation 
+
+
+            if(!$newCountry->save()){
+                //TODO: log this
+            }
+            
+        }
 
     }
 }
