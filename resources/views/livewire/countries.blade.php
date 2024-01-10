2c1edffe19984e1637fcaa19a5512078b2e78d7b
Michaela <kvapka.kvapla@gmail.com>
2024-01-10 17:01:45 +0100

    list of countries

--- resources/views/livewire/countries.blade.php
+++ resources/views/livewire/countries.blade.php
@@ -1,29 +1,35 @@
-<div>
-  
-    @if (session()->has('message'))
-        <div class="alert alert-success">
-            {{ session('message') }}
+
+
+
+        <div>
+        
+            @if (session()->has('message'))
+                <div class="alert alert-success">
+                    {{ session('message') }}
+                </div>
+            @endif
+        
+
+        
+            <table class="table table-bordered mt-5">
+                <thead>
+                    <tr>
+                        <th>No.</th>
+                        <th>Name</th>
+                        <th>Flag</th>
+                        <th width="150px">Action</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    @foreach($countries as $country)
+                    <tr>
+                        <td>{{ $country->id }}</td>
+                        <td>{{ $country->commonName }}</td>
+                        <td><img src='{{$country->flagUrl}}' width="40px"></td>
+                        <td></td>
+                    </tr>
+                    @endforeach
+                </tbody>
+            </table>
         </div>
-    @endif
-  
 
-  
-    <table class="table table-bordered mt-5">
-        <thead>
-            <tr>
-                <th>No.</th>
-               
-                <th width="150px">Action</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($countries as $country)
-            <tr>
-                <td>{{ $country->commonName }}</td>
-                <td>{{ $country->commonName }}</td>
-
-            </tr>
-            @endforeach
-        </tbody>
-    </table>
-</div>
