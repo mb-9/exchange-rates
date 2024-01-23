c0acccbe3727f5e4ef114aaae8ae259131e0b4d1
Michaela <kvapka.kvapla@gmail.com>
2024-01-23 21:34:25 +0100

    changed validation rule of currency-rate to numeric updated comments removed unnecessary functions

--- app/Models/Country.php
+++ app/Models/Country.php
@@ -6,13 +6,23 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
-
+/**
+ * @property string commonName
+ * @property string officialName
+ * @property string capital
+ * @property int population
+ * @property string timezone
+ * @property string flagUrl
+ * @property string currencyCode
+ * @property string currencySymbol
+ *
+ */
 class Country extends Model
 {
     use HasFactory;
 
 
-    public $validationRules = [
+    public array $validationRules = [
         'commonName'        => 'string',
         'officialName'      => 'required',
         'capital'           => 'string',
@@ -26,14 +36,13 @@ class Country extends Model
     /**
      * Validates the model
      *
-     * @return array array of error messages if the validation was not successful 
-     * @return bool true if validation has been successful 
+     * @return array|bool array of error messages if the validation was not successful, true if model is valid
      */
     public function validate() : array|bool
     {
 
         $validator  = Validator::make($this->getAttributes(), $this->validationRules);
-       
+
         if ($validator->fails()) {
             return $validator->messages()->jsonSerialize();
         }
@@ -41,14 +50,14 @@ class Country extends Model
         return true;
     }
 
-    
+
     /**
      * Sets model attributes from the result of calling api
      *
      * @param  array $country
      * @return void
      */
-    public function fillAttributesFromApiResult(array $country) : void 
+    public function fillAttributesFromApiResult(array $country) : void
     {
 
         $this->commonName     = $country['name']['common'] ?? NULL;
@@ -57,21 +66,21 @@ class Country extends Model
         $this->population     = $country['population'] ?? NULL;
         $this->timezone       = $country['timezones'][0] ?? NULL;
         $this->flagUrl        = $country['flags']['png'] ?? NULL;
-        
+
         if(isset($country['currencies']))
         {
 
             $arrKeys      = array_keys($country['currencies']);
             $currencyCode = $arrKeys[0] ?? NULL;
-            
+
             $this->currencyCode   = $currencyCode;
             $this->currencySymbol = $country['currencies'][$currencyCode]['symbol'] ?? NULL;
-        
+
         }
 
     }
 
-    
-  
+
+
 
 }
--- app/Models/Currency.php
+++ app/Models/Currency.php
@@ -6,37 +6,27 @@ use Illuminate\Database\Eloquent\Model;
 use Illuminate\Support\Facades\Validator;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 
+/**
+ * @property string currency
+ * @propert
+ */
 class Currency extends Model
 {
     use HasFactory;
 
-    public $validationRules = [
+    public array $validationRules = [
         'currency'  => 'required|string',
-        'rate'      => 'required|integer',
+        'rate'      => 'required|numeric',
     ];
-
-
-    /**
-     * Returns decimal 
-     */
-    public function rateDecimal(){
-        return $this->rate/100000;
-    } 
-
-    public function rateFormatted(){
-        return rtrim($this->rate, '0');
-    }
 
     /**
      * Validates the model
      *
-     * @return array array of error messages if the validation was not successful 
-     * @return bool true if validation has been successful 
+     * @return array|bool array of error messages if the validation was not successful, true if model is valid
      */
     public function validate() : array|bool{
 
         $validator  = Validator::make($this->getAttributes(), $this->validationRules);
-       
         if ($validator->fails()) {
             return $validator->messages()->jsonSerialize();
         }
@@ -45,5 +35,5 @@ class Currency extends Model
 
     }
 
-    
+
 }
